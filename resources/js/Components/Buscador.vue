f4ec216d38c6e6a4497af2218ab314ee5ac7bd60
Luis P <luispramirez21@gmail.com>
2022-12-27 08:17:25 -0600

    Cambios Luis

--- resources/js/Components/Buscador.vue
+++ resources/js/Components/Buscador.vue
@@ -27,7 +27,7 @@ defineExpose({ focus: () => input.value.focus() });
 
         </div>
         <input  type="search" name="search" placeholder="Buscar..."  ref="input"
-            :value="modelValue" @input="$emit('update:modelValue', $event.target.value)" class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"  required>
+            :value="modelValue" @input="$emit('update:modelValue', $event.target.value)" class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 "  required>
     </div>
 </div>
 
